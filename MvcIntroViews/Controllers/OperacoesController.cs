3aa01202f733ed4a17285ffea2d7729400b9bf42
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-09-06 22:15:17 -0300

    Adicionando operação de divisão

--- MvcIntroViews/Controllers/OperacoesController.cs
+++ MvcIntroViews/Controllers/OperacoesController.cs
@@ -25,4 +25,15 @@ public class OperacoesController : Controller
         ViewBag.Resultado = n1 - n2;
         return View();
     }
+
+    public IActionResult Dividir() {
+        return View();
+    }
+
+    public IActionResult ResultadoDivisao(double n1, double n2) {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = (n1/n2);
+        return View();
+    }
 }
